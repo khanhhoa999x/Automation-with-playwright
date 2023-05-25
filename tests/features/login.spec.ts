7d2bf051e94efacfc207d2bb883ee572674f5b3b
khanhhoa999x <khanhhoabmt9.9.9x@gmail.com>
2023-05-25 15:30:57 +0700

    separate Login Page

--- tests/features/login.spec.ts
+++ tests/features/login.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { homePage } from "../pages/homePage";
+import { loginPage } from "../pages/loginPage";
 
 const users = require("../data/Users.json");
 const validUserArray = users.validUser;
@@ -7,12 +8,11 @@ const validUserArray = users.validUser;
 validUserArray.forEach((user) => {
   test("Should login with email `{$user.email}`", async ({ page }) => {
     const HomePage = new homePage(page);
+    const LoginPage = new loginPage(page);
     await HomePage.Navigate_to_home_page();
     await HomePage.Navigate_to_login_page();
-    await page.getByLabel("*Email Address").click();
-    await page.getByLabel("*Email Address").fill(user.email);
-    await page.getByLabel("*Password").click();
-    await page.getByLabel("*Password").fill(user.password);
-    await page.getByRole("button", { name: "Login" }).click();
+    await LoginPage.Type_email(user.email);
+    await LoginPage.Type_password(user.password);
+    await LoginPage.Click_login();
   });
 });
---
+++ tests/pages/loginPage.ts
@@ -0,0 +1,29 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class loginPage {
+    readonly page:Page;
+    readonly emailTxtBox;
+    readonly passwordTxtBox;
+    readonly loginBttn;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.emailTxtBox = page.getByLabel("*Email Address");
+        this.passwordTxtBox = page.getByLabel("*Password");
+        this.loginBttn = page.getByRole("button", { name: "Login" });
+    }
+    
+    async Type_email(email) {
+        await this.emailTxtBox.click();
+        await this.emailTxtBox.fill(email);
+    }
+
+    async Type_password(password) {
+        await this.passwordTxtBox.click();
+        await this.passwordTxtBox.fill(password);
+    }
+
+    async Click_login() {
+        await this.loginBttn.click();
+    }
+}
