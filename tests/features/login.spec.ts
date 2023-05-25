b4c1151b37e69ef858b391216f454e0a008a29c2
khanhhoa999x <khanhhoabmt9.9.9x@gmail.com>
2023-05-25 11:27:45 +0700

    call HomePage's method

--- tests/features/login.spec.ts
+++ tests/features/login.spec.ts
@@ -7,8 +7,8 @@ const validUserArray = users.validUser;
 validUserArray.forEach((user) => {
   test("Should login with email `{$user.email}`", async ({ page }) => {
     const HomePage = new homePage(page);
-    await HomePage.Navigate_to_home_page;
-    await HomePage.Navigate_to_login_page;
+    await HomePage.Navigate_to_home_page();
+    await HomePage.Navigate_to_login_page();
     await page.getByLabel("*Email Address").click();
     await page.getByLabel("*Email Address").fill(user.email);
     await page.getByLabel("*Password").click();
